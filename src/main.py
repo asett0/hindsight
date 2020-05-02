cae2e00e4c7324161e39b9c6c34ee34cfa69f369
amit <amitsett0@gmail.com>
2020-05-02 10:25:23 +0800

    Added json file that contains working symols for av api

--- src/main.py
+++ src/main.py
@@ -5,5 +5,19 @@ import os
 # import pandas
 
 load_dotenv()
-print(os.getenv("ALPHA_ADVANTAGE_API_KEY"))
-# ts = TimeSeries(key)
+# key = os.getenv("ALPHA_ADVANTAGE_API_KEY")
+ts = TimeSeries(key= os.getenv("ALPHA_ADVANTAGE_API_KEY"), output_format="pandas")
+
+df, metadata = ts.get_daily(symbol="ASX:VAS", outputsize='full')
+
+print(df)
+# print(metadata)
+# for row in df:
+#     print(row)
+#     print("\n")
+# print(df)
+# df.map()
+
+# print(type(df))
+
+# print(df.shape())
