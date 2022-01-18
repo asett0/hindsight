bf7d95cbf1a2d057c24fdc4992c54b59fe85bd8d
asett0 <amitsett0@gmail.com>
2022-01-19 07:22:30 +0800

    checkpoint to switch laptops

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,27 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-// type Data = Vec<f64>;
 type Item = &'static str;
-type Portfolio = HashMap<Item, f64>;
+type Quantity = f64;
+type Portfolio = HashMap<Item, Quantity>;
+type Tick = HashSet<Price>;
+type UUID = u32;
 
-// enum Order {
-//     MarketOrder,
-//     LimitOrder,
-// }
+#[derive(PartialEq, Eq)]
+struct ExchangablePair {
+    tx: Item,
+    rx: Item,
+}
+
+// Price struct which represents how much tx you need to spend for one unit of rx
+struct Price {
+    ex: ExchangablePair,
+    amt: Quantity,
+}
+
+enum Order {
+    MarketOrder { ex: ExchangablePair, amt: Quantity },
+    LimitOrder { price: Price, amt: Quantity },
+}
 
 // #[derive(PartialEq, Eq, Hash)]
 // enum Item {
@@ -20,10 +34,10 @@ type Portfolio = HashMap<Item, f64>;
 // impl Order {
 //     fn submit_order(
 //         order: Order,
-//         give: Item,
-//         receive: Item,
-//         give_amt: f64,
-//         receive_amt: f64,
+//         tx: Item,
+//         rx: Item,
+//         tx_amt: f64,
+//         rx_amt: f64,
 //         portfolio: &mut Portfolio,
 //     ) {
 //         match order {
@@ -36,13 +50,81 @@ type Portfolio = HashMap<Item, f64>;
 //     }
 // }
 
-fn trade(give: Item, receive: Item, give_amt: f64, receive_amt: f64, portfolio: &mut Portfolio) {
-    let amt = portfolio.entry(give).or_insert(0.);
-    *amt -= give_amt;
+fn trade(
+    ExchangablePair { tx, rx }: ExchangablePair,
+    tx_amt: f64,
+    rx_amt: f64,
+    portfolio: &mut Portfolio,
+) {
+    let amt = portfolio.entry(tx).or_insert(0.);
+    *amt -= tx_amt;
 
-    let amt = portfolio.entry(receive).or_insert(0.);
-    *amt += receive_amt;
+    let amt = portfolio.entry(rx).or_insert(0.);
+    *amt += rx_amt;
 }
+
+fn fill(tick: Tick, orders: &mut HashMap<UUID, Order>, portfolio: &mut Portfolio) {
+    let filled_orders = Vec::new();
+
+    for (uuid, order) in orders.iter() {
+        match order {
+            Order::MarketOrder {
+                ex: order_ex,
+                amt: order_amt,
+            } => {
+                for Price {
+                    ex: price_ex,
+                    amt: price_amt,
+                } in tick
+                {
+                    if price_ex == order_ex {
+                        trade(order_ex, price_amt * order_amt, order_amt, portfolio);
+                        filled_orders.push(&order);
+                    }
+                }
+            }
+            Order::LimitOrder {
+                price:
+                    Price {
+                        ex: order_ex,
+                        amt: limit,
+                    },
+                amt: order_amt,
+            } => {
+                for Price {
+                    ex: price_ex,
+                    amt: price_amt,
+                } in tick
+                {
+                    if (price_ex == order_ex) && (price_amt <= limit) {
+                        trade(order_ex, price_amt * order_amt, order_amt, portfolio);
+                        filled_orders.push(&order);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn is_fill(
+    Price {
+        ex: price_ex,
+        amt: price_amt,
+    }: Price,
+    order: Order,
+) -> bool {
+    match order {
+        Order::MarketOrder { ex: order_ex, .. } => price_ex == order_ex,
+        Order::LimitOrder {price: Price {ex: order_ex, amt: order_amt},.. } =>}
+    }
+}
+// fn backtest(ticks: Vec<HashMap<Item, f64>>) {
+//     let mut orders: HashSet<Order> = HashSet::new();
+
+//     for tick in ticks {
+//         MarketOrder
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -51,9 +133,26 @@ mod tests {
     #[test]
     fn trade_aud_bitcoin() {
         let mut portfolio = HashMap::from([("AUD", 1000.)]);
-        trade("AUD", "BITCOIN", 100., 1., &mut portfolio);
+        let ex = ExchangablePair {
+            tx: "AUD",
+            rx: "BITCOIN",
+        };
+        trade(ex, 100., 1., &mut portfolio);
 
         assert!((portfolio.get("AUD").unwrap() - 900.).abs() < 0.000001);
         assert!((portfolio.get("BITCOIN").unwrap() - 1.).abs() < 0.000001);
+    }
+
+    #[test]
+    fn short_aud_microsoft() {
+        let mut portfolio = HashMap::new();
+        let ex = ExchangablePair {
+            tx: "AUD",
+            rx: "MICROSOFT",
+        };
+        trade(ex, 100., 1., &mut portfolio);
+
+        assert!((portfolio.get("AUD").unwrap() + 100.).abs() < 0.000001);
+        assert!((portfolio.get("MICROSOFT").unwrap() - 1.).abs() < 0.000001);
     }
 }
