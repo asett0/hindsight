0539528afc0baffc76696c47011f09d32c751f60
setta <setta@cbh.com.au>
2022-01-19 17:51:50 +0800

    renamed tick stepping to market event

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use std::{
 type Item = &'static str;
 type Quantity = f64;
 type Portfolio = HashMap<Item, Quantity>;
-type Tick = HashSet<Price>;
+type MarketEvent = HashSet<Price>;
 type UUID = u32;
 
 #[derive(PartialEq, Eq, Hash, Debug)]
@@ -102,8 +102,12 @@ fn try_fill(
     }
 }
 
-fn tick_step(tick: Tick, orders: &mut Vec<Order>, portfolio: &mut Portfolio) {
-    for price in tick.iter() {
+fn market_event_step(
+    market_event: MarketEvent,
+    orders: &mut Vec<Order>,
+    portfolio: &mut Portfolio,
+) {
+    for price in market_event.iter() {
         orders.retain(|order| !try_fill(price, order, portfolio));
     }
 }
@@ -329,7 +333,7 @@ mod tests {
     }
 
     #[test]
-    fn tick_step_market_executed() {
+    fn market_event_step_market_executed() {
         let eps = 0.000001;
         let price = Price {
             ex: ExchangablePair {
@@ -349,11 +353,11 @@ mod tests {
             order_type: OrderType::MarketOrder,
         };
 
-        let tick = HashSet::from([price]);
+        let market_event = HashSet::from([price]);
         let mut orders = vec![order];
         let mut portfolio = HashMap::new();
 
-        tick_step(tick, &mut orders, &mut portfolio);
+        market_event_step(market_event, &mut orders, &mut portfolio);
 
         assert!(orders.is_empty());
         assert!((portfolio.get("AUD").unwrap() + 1.).abs() < eps);
@@ -361,7 +365,7 @@ mod tests {
     }
 
     #[test]
-    fn tick_step_market_not_executed() {
+    fn market_event_step_market_not_executed() {
         let price = Price {
             ex: ExchangablePair {
                 tx: "AUD",
@@ -380,18 +384,18 @@ mod tests {
             order_type: OrderType::MarketOrder,
         };
 
-        let tick = HashSet::from([price]);
+        let market_event = HashSet::from([price]);
         let mut orders = vec![order];
         let mut portfolio = HashMap::new();
 
-        tick_step(tick, &mut orders, &mut portfolio);
+        market_event_step(market_event, &mut orders, &mut portfolio);
 
         assert_eq!(orders[0].uuid, 1);
         assert!(portfolio.is_empty());
     }
 
     #[test]
-    fn tick_step_limit_executed() {
+    fn market_event_step_limit_executed() {
         let eps = 0.000001;
         let price = Price {
             ex: ExchangablePair {
@@ -411,11 +415,11 @@ mod tests {
             order_type: OrderType::LimitOrder(2.),
         };
 
-        let tick = HashSet::from([price]);
+        let market_event = HashSet::from([price]);
         let mut orders = vec![order];
         let mut portfolio = HashMap::new();
 
-        tick_step(tick, &mut orders, &mut portfolio);
+        market_event_step(market_event, &mut orders, &mut portfolio);
 
         assert!(orders.is_empty());
         assert!((portfolio.get("AUD").unwrap() + 1.).abs() < eps);
@@ -423,7 +427,7 @@ mod tests {
     }
 
     #[test]
-    fn tick_step_limit_not_executed() {
+    fn market_event_step_limit_not_executed() {
         let price = Price {
             ex: ExchangablePair {
                 tx: "AUD",
@@ -442,11 +446,11 @@ mod tests {
             order_type: OrderType::LimitOrder(0.5),
         };
 
-        let tick = HashSet::from([price]);
+        let market_event = HashSet::from([price]);
         let mut orders = vec![order];
         let mut portfolio = HashMap::new();
 
-        tick_step(tick, &mut orders, &mut portfolio);
+        market_event_step(market_event, &mut orders, &mut portfolio);
 
         assert_eq!(orders[0].uuid, 1);
         assert!(portfolio.is_empty());
